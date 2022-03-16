e0f649d7d1ad4a80874274f6af4a838dc8bd3697
Eduardo Addad de Oliveira <duduaddad@gmail.com>
2022-03-16 19:22:05 -0300

    Aula 2

---
+++ src/main/java/com/example/exemplo1/controlles/MinhaConfiguracao.java
@@ -0,0 +1,11 @@
+package com.example.exemplo1.controlles;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguracao {
+    public String nomeAplicacao(){
+        return "Aula 2 - Teste";
+    }
+    
+}
