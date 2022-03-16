92687a9483665755104584ea55487ee56b7c5f3c
Eduardo Addad de Oliveira <duduaddad@gmail.com>
2022-03-16 19:43:54 -0300

    Aula 2 - Atualização 2

--- src/main/java/com/example/exemplo1/controlles/HelloControlles.java
+++ src/main/java/com/example/exemplo1/controlles/HelloControlles.java
@@ -1,12 +1,26 @@
 package com.example.exemplo1.controlles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloControlles {
+    @Autowired
+    @Qualifier("nomeAplicacao")
+    private String nomeAplicacao;
+
+    @Autowired
+    @Qualifier("versaoAplicacao")
+    private String versaoAplicacao;
+
     @GetMapping(value="/")
     public String getMethodName() {
-        return "Olá Eduardo";
+        return nomeAplicacao + " - " + versaoAplicacao;
+
+   // @GetMapping(value="/")
+   // public String getMethodName() {
+   //     return "Olá Eduardo";
     }
     
     
--- src/main/java/com/example/exemplo1/controlles/MinhaConfiguracao.java
+++ src/main/java/com/example/exemplo1/controlles/MinhaConfiguracao.java
@@ -1,11 +1,16 @@
 package com.example.exemplo1.controlles;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguracao {
+    @Bean
     public String nomeAplicacao(){
         return "Aula 2 - Teste";
     }
-    
+    @Bean
+    public String versaoAplicacao(){
+        return "1.0.0";
+    }
 }
