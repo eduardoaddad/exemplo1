be99d279d0496e9eaed827ce28368e8a2d26b467
Eduardo Addad de Oliveira <duduaddad@gmail.com>
2022-03-16 22:18:15 -0300

    Aula 2 Atualização

--- src/main/java/com/example/exemplo1/controlles/MinhaConfiguracao.java
+++ src/main/java/com/example/exemplo1/controlles/MinhaConfiguracao.java
@@ -1,18 +1,26 @@
 package com.example.exemplo1.controlles;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguracao {
-    @Bean
-    public String nomeAplicacao()
-    {
+    @Bean(name = "NomeAplicacao")
+    public String nomeAplicacao(){
         return "Aula 2 - Teste";
     }
-    @Bean
-    public String versaoAplicacao()
-    {
+    @Bean(name = "versaoAplicacao")
+    public String versaoAplicacao(){
         return "1.0.0";
+    }
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args ->{
+            System.out.println("Executou com método!!!");
+        };
     }
 }
